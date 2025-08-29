95b8100c5103b8fa339abbd668950a6ac21dd26b
RTSolutionsLarry <larry.randolph@randolphtechnologysolutions.com>
2025-08-29 13:00:37 -0700

    Button setup done via JS instead of HTML

--- main.js
+++ main.js
@@ -14,15 +14,8 @@ const createInputObjects = (inputs) => {
     }
 }
 
-const makeButtonsClickable = (inputs) => {
-    for (input of inputs) {
-        input.addEventListener('click',buttonPress(input.textContent));
-
-    }
-}
-
 const buttonPress = (textContent) => {
-    //console.log(`In buttonPress function: ${textContent}`);
+    console.log(`In buttonPress function: ${textContent}`);
 }
 
 const operate = (listOfButtonsPressed) => {
@@ -34,22 +27,104 @@ const clear = () => {
 }
 
 const addButtonDetails = (button, x, y) => {
-    console.log('start back here. Make a switch statement that looks at x and y and adds correct classes and onClick function to buttons')
+    switch (true) {
+        case (x === 1) && (y ===1):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 7;
+            break;
+        case (x === 2) && (y ===1):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 8;
+            break;
+        case (x === 3) && (y ===1):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 9;
+            break;
+        case (x === 4) && (y ===1):
+            button.classList.add('lightGray');
+            button.classList.add('operator');
+            button.innerText = '/';
+            break;
+        case (x === 1) && (y ===2):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 4;
+            break;
+        case (x === 2) && (y ===2):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 5;
+            break;
+        case (x === 3) && (y ===2):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 6;
+            break;
+        case (x === 4) && (y ===2):
+            button.classList.add('lightGray');
+            button.classList.add('operator');
+            button.innerText = '*';
+            break;
+        case (x === 1) && (y ===3):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 1;
+            break;
+        case (x === 2) && (y ===3):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 2;
+            break;
+        case (x === 3) && (y ===3):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 3;
+            break;
+        case (x === 4) && (y ===3):
+            button.classList.add('lightGray');
+            button.classList.add('operator');
+            button.innerText = '-';
+            break;
+        case (x === 1) && (y ===4):
+            button.classList.add('darkGray');
+            button.classList.add('number');
+            button.innerText = 0;
+            break;
+        case (x === 2) && (y ===4):
+            button.classList.add('gray');
+            button.classList.add('clear');
+            button.innerText = 'C';
+            break;
+        case (x === 3) && (y ===4):
+            button.classList.add('green');
+            button.classList.add('equals');
+            button.innerText = '=';
+            break;
+        case (x === 4) && (y ===4):
+            button.classList.add('lightGray');
+            button.classList.add('operator');
+            button.innerText = '+';
+            break;                                           
+    }
 }
 
 const buttonSetup = () => {
     const inputsContainer = document.querySelector('.inputsContainer');
-    for (let y = 0; y < 4; y++) {
+    for (let y = 1; y < 5; y++) {
         const divRow = document.createElement('div');
         divRow.classList = 'inputRow';
         inputsContainer.appendChild(divRow);
-        for (let x = 0; x < 4; x++) {
+        for (let x = 1; x < 5; x++) {
             const button = document.createElement('button');
-            addButtonDetails(button, x, y); 
+            button.classList.add('input');
+            addButtonDetails(button, x, y);
+            divRow.appendChild(button); 
         }
     }
 }
 
-createInputObjects(inputs)
-console.log(inputs);
+//createInputObjects(inputs)
 buttonSetup();
